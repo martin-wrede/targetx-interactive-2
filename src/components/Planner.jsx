0dd0d134340a5bbf8824798be9dd9588bed172b0
martin-wrede <martin.wrede@targetx.de>
2025-07-10 16:34:45 +0200

    iframe neu

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext,  useRef, useEffect, useState} from 'react';
 import Gallery from './Gallery';
 
 import { Context } from '../Context';
@@ -9,37 +9,27 @@ export default function Planner() {
   const {lang} = useContext(Context);
     const { language } = useContext(Context); // ❌ This returns `undefined`
   
+     
 
- 
-
- 
   return (
  <div>
     
   
-    
-  
+  <iframe
+     src={`https://react-chatbot-air-prompt-2.pages.dev/?part2=none&lang=${language}`}
+      title="External Content"
 
-            <iframe
-            src={`https://react-chatbot-air-prompt-2.pages.dev/?part2=none&lang=${language}`}
-
-            title="External Content"
-            width="100%"
-             height="100%"
-          //  height="600"
-
-          style={{
-   // position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh',
-    border: 'none',
-  }}
-           
-           // style={{ border: '1px solid #ccc' }}
-          ></iframe>
-
+     style={{
+        position: 'relative',   // oder absolute, je nach deinem Layout
+        top: 40,
+        left: 0,
+        width: '100vw',
+        height: '80vh', // calc('100vh' - '40'),     // volle Browserhöhe
+        border: 'none',      // oder dein gewünschter Rahmen
+      //  overflow: 'hidden'   // verhindert innere Scrollbars
+      }}
+     // scrolling="no"
+    />
        
        <br />
          <br />
